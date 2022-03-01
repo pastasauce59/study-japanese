f707cb5bdcbb258ad02bf516801c9901eeda1cbf
Michael Primavera <pastasauce59@gmail.com>
2022-03-01 16:51:17 -0500

    created Hiragana component and established routing, added minor css to prevent overlap with NavigationBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavigationBar from "./Components/NavigationBar";
+import Hiragana from './Components/Hiragana';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+
+      <Routes>
+        <Route exact path='/hiragana' element={<Hiragana />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Hiragana.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hiragana(props) {
+    return (
+        <div>
+            Hiragana Table of Characters Placeholder
+        </div>
+    );
+}
+
+export default Hiragana;
