2f1cf466879121041176ac96188858047c992ba2
Michael Primavera <pastasauce59@gmail.com>
2022-03-06 20:05:33 -0500

    added flip animation to flip card and reveal its translated character, added logic to cycle through cards

--- src/Components/HiraganaStudy.js
+++ src/Components/HiraganaStudy.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { hiraganaAll } from './Hiragana'
 
 function HiraganaStudy(props) {
 
+    const [hiragana, setHiragana] = useState(hiraganaAll)
+    const [count, setCount] = useState(0)
+    const [flip, setFlip] = useState(null)
+
+    let plusMinusCount = () => {
+        setCount(count + 1)
+    }
+
+    let handleFlip = () => {
+        flip === null ? setFlip({transform: 'rotateY(180deg)'}) : setFlip(null)
+    }
+
+
+
     return (
         <div className='card-container'>
-            <div className='card'>
-                {hiraganaAll[0].character}
+            <div onClick={handleFlip} className='card' style={flip}>
+                <div className='front-card'>
+                    <hr/>
+                    {hiragana[count].character}
+                    <hr/>
+                </div>
+
+                <div className='back-card'>
+                    <hr/>
+                    {hiragana[count].key}
+                    <hr/>
+                </div>
+            </div>
+
+            <div>
+                <button onClick={plusMinusCount}>Next</button>
             </div>
             
         </div>
@@ -14,3 +42,5 @@ function HiraganaStudy(props) {
 }
 
 export default HiraganaStudy;
+
+// hiraganaAll[Math.floor(Math.random() * (70 - 0))]
