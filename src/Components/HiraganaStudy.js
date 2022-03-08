a0c0da7fce77774913f54b59c82826d33b617da6
Michael Primavera <pastasauce59@gmail.com>
2022-03-07 20:58:37 -0500

    added counter and logic to show which card you are currently viewing out of all hiragana, added info paragraph, changed (temporary) buttons, added styling for page to give a better blueprint of what final will look like

--- src/Components/HiraganaStudy.js
+++ src/Components/HiraganaStudy.js
@@ -7,8 +7,22 @@ function HiraganaStudy(props) {
     const [count, setCount] = useState(0)
     const [flip, setFlip] = useState(null)
 
-    let plusMinusCount = () => {
+    let plusMinusCount = (e) => {
+        if (e.target.textContent === 'Next'){
+            if (count === 70){
+                return
+            } else {
         setCount(count + 1)
+            }
+        }
+        if (e.target.textContent === 'Previous'){
+            if (count === 0){
+                return
+            } else {
+            setCount(count - 1)
+            }
+        }
+        // console.log(e.target.textContent)
     }
 
     let handleFlip = () => {
@@ -18,26 +32,42 @@ function HiraganaStudy(props) {
 
 
     return (
-        <div className='card-container'>
-            <div onClick={handleFlip} className='card' style={flip}>
-                <div className='front-card'>
-                    <hr/>
-                    {hiragana[count].character}
-                    <hr/>
-                </div>
+        <div className='study-container'>
 
-                <div className='back-card'>
-                    <hr/>
-                    {hiragana[count].key}
-                    <hr/>
+            <div className='number-of'>
+            <h3>{`${hiragana.indexOf(hiragana[count]) + 1} out of ${hiragana.length}`}</h3>
+            </div>
+
+            <div className='study-info'>
+                <p>ℹ️ Click on the card to the right to reveal the characters translation!</p>
+            </div>
+
+            <div className='card-container'>
+                <div onClick={handleFlip} className='card' style={flip}>
+                    <div className='front-card'>
+                        <hr/>
+                        {hiragana[count].character}
+                        <hr/>
+                    </div>
+
+                    <div className='back-card'>
+                        <hr/>
+                        {hiragana[count].key}
+                        <hr/>
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <button onClick={plusMinusCount}>Next</button>
+            <div className='buttons'>
+                <div className='button'>
+                    <button onClick={plusMinusCount}>Next</button>
+                </div>
+
+                <div className='button'>
+                    <button onClick={plusMinusCount}>Previous</button>
+                </div>
             </div>
-            
-        </div>
+    </div>
     );
 }
 
