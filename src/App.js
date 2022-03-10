a2a0e72ede3bb81d6589d77951c3e9d83988dff8
Michael Primavera <pastasauce59@gmail.com>
2022-03-10 18:45:57 -0500

    created a HiraganaQuiz component with basic layout, added an 'about' section for homepage, added styling for app and study components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import NavigationBar from "./Components/NavigationBar";
 import HiraganaCharacters from './Components/HiraganaCharacters';
 import HiraganaStudy from './Components/HiraganaStudy';
+import HiraganaQuiz from './Components/HiraganaQuiz';
 
 function App() {
   return (
@@ -12,9 +13,30 @@ function App() {
       <NavigationBar />
 
       <Routes>
+        <Route exact path='/' element={
+          <div className='homepage'>
+            <h1 className='about'>Welcome to Study🇯🇵Japanese!</h1>
+            <h4 className='about'>A little bit about this app:</h4>
+            <div className='about'>
+            <p >In order to study any language, you first must start with learning the foundation that
+              is the target language's alphabet. I created this app with that goal in mind. As someone who is
+              fascinated with Japanese culture, I have taken up an interest in learning the language. That's when I came
+              to learn Japanese kind of has three alphabet's, or three different writing systems, that work together as a whole.
+              </p>
+              <p >
+              In this app I am focused on learning Hiragana and Katakana (because kanji is a different beast) by having 
+              character tables to refernece, index-like cards to study and finally a quiz to see how many characters can be 
+              correctly recalled. I wanted to create an app that I found easy to use, simple and straightforward at what it's
+              trying to achieve, and all while being an enjoyable user experience.
+            </p>
+            </div>
+          </div>
+        } />
         <Route exact path='/hiragana' element={<HiraganaCharacters />} />
         <Route exact path='/hiragana/study' element={<HiraganaStudy />} />
+        <Route exact path='/hiragana/quiz' element={<HiraganaQuiz />} />
       </Routes>
+
     </div>
   );
 }
---
+++ src/Components/HiraganaQuiz.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+function HiraganaQuiz(props) {
+
+    
+    return (
+        <div>
+            <h1>inside hiragana quiz</h1>
+            <Navbar fixed='bottom' bg='light' variant='dark'>
+                
+                <h1>a</h1>
+                <h1>ka</h1>
+                <h1>shi</h1>
+                <h1>so</h1>
+                
+            </Navbar>
+        </div>
+    );
+}
+
+export default HiraganaQuiz;
