12c06291071a55d81f33f09203b929a26259a090
Michael Primavera <pastasauce59@gmail.com>
2022-03-11 21:42:40 -0500

    modified quiz bar and adding styling to give a visual of what multiple choice selection will look like

--- src/Components/HiraganaQuiz.js
+++ src/Components/HiraganaQuiz.js
@@ -3,18 +3,20 @@ import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 function HiraganaQuiz(props) {
 
-    
+    let handleClick = (e) => {
+        console.log(e.target.textContent)
+    }
+
     return (
-        <div>
+        <div className='quiz-container'>
             <h1>inside hiragana quiz</h1>
-            <Navbar fixed='bottom' bg='light' variant='dark'>
-                
+            {/* <Navbar fixed='bottom' bg='light' variant='dark'> */}
+            <div onClick={handleClick} className='quiz-bar'>   
                 <h1>a</h1>
                 <h1>ka</h1>
                 <h1>shi</h1>
                 <h1>so</h1>
-                
-            </Navbar>
+            </div>    
         </div>
     );
 }
