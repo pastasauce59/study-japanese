e6ba48f022c252e8ef8c60f7873ef41b3f6f30cb
Michael Primavera <pastasauce59@gmail.com>
2022-03-16 10:47:18 -0400

    added logic to multiple choice to include correct answer and three other randomize answers, and then randomize the multiple choice order

--- src/Components/HiraganaQuiz.js
+++ src/Components/HiraganaQuiz.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 import { hiraganaAll } from './Hiragana';
 
+
 function HiraganaQuiz(props) {
 
     const [hiragana, setHiragana] = useState(hiraganaAll)
     const [count, setCount] = useState(0)
     const [correct, setCorrect] = useState(0)
     const [incorrect, setIncorrect] = useState(0)
-
-    let OG = [...hiraganaAll]
-
-    let hiraganaKeys = []
-    OG.map( hiragana => hiraganaKeys.push(hiragana.key))
 
     let randomize = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -22,13 +18,40 @@ function HiraganaQuiz(props) {
           }
     }
 
+    randomize(hiragana)
+
+
+    let OG = [...hiraganaAll]
+
+    let hiraganaKeys = []
+
+    hiragana.map( h => hiraganaKeys.push(h.key))
+
+    let quizbarKeys = [
+        hiragana[count].key,
+        hiragana[Math.floor(Math.random() * 71)].key,
+        hiragana[Math.floor(Math.random() * 71)].key,
+        hiragana[Math.floor(Math.random() * 71)].key
+    ]
+
+    randomize(quizbarKeys)
+
+    // let randomize = (array) => {
+    //     for (let i = array.length - 1; i > 0; i--) {
+    //         const j = Math.floor(Math.random() * (i + 1));
+    //         const temp = array[i];
+    //         array[i] = array[j];
+    //         array[j] = temp;
+    //       }
+    // }
+
     let handleClick = (e) => {
         console.log(e.target.textContent)
+        if(e.target.textContent === hiragana[count].key){
+            console.log('correct')
+        } else console.log('WRONG')
     }
 
-    randomize(hiragana)
-    // console.log(hiragana)
-    console.log(hiraganaKeys)
 
     return (
         <div className='quiz-container'>
@@ -59,10 +82,13 @@ function HiraganaQuiz(props) {
             </div>
             
             <div onClick={handleClick} className='quiz-bar'>   
-                <h1>{hiragana[count].key}</h1>
+                {/* <h1>{hiragana[count].key}</h1>
                 <h1>ka</h1>
                 <h1>shi</h1>
-                <h1>so</h1>
+                <h1>so</h1> */}
+                {quizbarKeys.map(key => 
+                    <h1>{key}</h1>
+                    )}
             </div>    
         </div>
     );
