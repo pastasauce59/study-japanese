00dd8f40c7edffaa6167314169bb29611165c645
Michael Primavera <pastasauce59@gmail.com>
2022-04-01 19:34:41 -0400

    added a quiz completion message at the end of quiz giving a percentage of correct answers and added buttons with options of taking quiz again or to study hiragana further

--- src/Components/HiraganaQuiz.js
+++ src/Components/HiraganaQuiz.js
@@ -6,12 +6,13 @@ function HiraganaQuiz(props) {
 
     const [hiragana, setHiragana] = useState(randomized)
     const [shuffle, setShuffle] = useState(false)
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(65)
     const [correct, setCorrect] = useState(0)
     const [incorrect, setIncorrect] = useState(0)
     const [flip, setFlip] = useState(null)
     const [style, setStyle] = useState(null)
     const [style2, setStyle2] = useState(null)
+    const [hidden, setHidden] = useState({visibility: 'hidden'})
 
     let randomize = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -64,67 +65,96 @@ function HiraganaQuiz(props) {
             setStyle(null) 
             setStyle2(null)
             setQuizKeys(nextChoices)
-            setTimeout(() => {setCount(count + 1)}, 300) 
+            if (count + 1 === 70){
+                setShuffle(false)
+                setStyle({visibility: 'hidden'})
+                setHidden(null)
+            } else {
+            setTimeout(() => {setCount(count + 1)}, 300) }
         }, 2000)
     }
 
-    let beginQuiz = () => {
+    let beginQuiz = (e) => {
+        if(e.target.textContent === 'Quiz Me Again!'){
+            window.location = 'http://localhost:3000/hiragana/quiz'
+        } else if (e.target.textContent === 'Study Hiragana'){
+            window.location = 'http://localhost:3000/hiragana/study'
+        } else
+        
         setShuffle(true);
         setQuizKeys(choices)
         randomize(quizKeys)
+        
     }
 
+    console.log(count)
 
     return (
         <div className='quiz-container'>
             {shuffle === false ? 
+
                 <div className='begin-quiz-container'>
-                    <div onClick={beginQuiz} className='begin-quiz'>
+                    <div onClick={beginQuiz} style={style}className='begin-quiz'>
                         Begin Quiz
                     </div>
-                </div> :  <div>
-            
-            <div className='number-of'>
-                <h3>{`${hiragana.indexOf(hiragana[count]) + 1} out of ${hiragana.length}`}</h3>
-            </div>
 
-            <div className='correct-count'>
-                <h4 className='correct'>Correct: {correct}</h4>
-                <h4 className='incorrect'>Incorrect: {incorrect}</h4>
-            </div>
-
-            <div className='card-container'>
-                <div className='card' style={flip}>
-                    <div className='front-card'>
-                        <hr/>
-                        {hiragana[count].character}
-                        {/* {ques} */}
-                        <hr/>
+                    <div className='quiz-complete' style={hidden}>
+                        <h1>{`You've completed the quiz and got ${Math.floor((correct / hiragana.length) * 10000) / 100}% correct!`}</h1>
+                        <div className='buttons'>
+                            <div onClick={beginQuiz} style={null}className='button'>
+                            Quiz Me Again!
+                            </div>
+                            <div onClick={beginQuiz} className='button'>
+                            Study Hiragana
+                            </div>
+                        </div>
                     </div>
-
-                    <div className='back-card'>
-                        <hr/>
-                        {hiragana[count].key}
-                        {/* {answer} */}
-                        <hr/>
-                    </div>
-                </div>
-            </div>
-            
-            <div className='quiz-bar'>   
+                </div> 
                 
-                {quizKeys.map(key => key === hiragana[count].key ? 
-                <h1 onClick={handleClick} style={style}>{key}</h1> : <h1 onClick={handleClick} style={style2}>{key}</h1>
-                )}
+                : 
+                
+                <div>
+            
+                    <div className='number-of'>
+                        <h3>{`${hiragana.indexOf(hiragana[count]) + 1} out of ${hiragana.length}`}</h3>
+                    </div>
 
-            </div>  
+                    <div className='correct-count'>
+                        <h4 className='correct'>Correct: {correct}</h4>
+                        <h4 className='incorrect'>Incorrect: {incorrect}</h4>
+                    </div>
 
-            </div> }
+                    <div className='card-container'>
+                        <div className='card' style={flip}>
+                            <div className='front-card'>
+                                <hr/>
+                                {hiragana[count].character}
+                                {/* {ques} */}
+                                <hr/>
+                            </div>
+
+                            <div className='back-card'>
+                                <hr/>
+                                {hiragana[count].key}
+                                {/* {answer} */}
+                                <hr/>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className='quiz-bar'>   
+                        
+                        {quizKeys.map(key => key === hiragana[count].key ? 
+                        <h1 onClick={handleClick} style={style}>{key}</h1> : <h1 onClick={handleClick} style={style2}>{key}</h1>
+                        )}
+
+                    </div>  
+
+                </div> 
+            }
 
         </div>
     );
 }
 
 export default HiraganaQuiz;
-
-// if (count > 72){ render a pertenage of questions correct page/ try again}
