35fa6d2a827025644d2c7fd76802a66cf83de916
Michael Primavera <pastasauce59@gmail.com>
2024-06-30 19:51:34 -0400

    installed packages and setup Server.js successfully.

--- backend/Server.js
+++ backend/Server.js
@@ -1,1 +1,19 @@
-// a bunch of code is going to go here very soon
+const express = require("express")
+
+const mongoose = require("mongoose")
+
+require("dotenv").config()
+
+const cors = require("cors")
+
+const app = express()
+const PORT = process.env.PORT | 5001
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Testing testing 1 2 3...")
+})
+
+app.listen(PORT, () => console.log(`listening @ ${PORT}`))
