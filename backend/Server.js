83b916e7aa00da042a691380656e44ff27461e15
Michael Primavera <pastasauce59@gmail.com>
2024-07-15 21:56:59 -0400

    added jsonwebtoken package to backend side, added jwt.sign to login method in UserControllers.

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const routes = require("./routes/UserRoute")
