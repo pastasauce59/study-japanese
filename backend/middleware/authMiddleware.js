5a47c1735571e8b118702ecdcdcc530fd0f67ede
Michael Primavera <pastasauce59@gmail.com>
2024-07-24 21:12:37 -0400

    Created authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split('')[1]
+
+    if (!token) {
+        return res.sendStatus(401).json({message: "No Token Authorized"})
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        
+        if (err) {
+            return res.sendStatus(403).json({message: "Invalid token, forbidden"})
+        }
+
+        req.user = user
+        next()
+    })
+}
